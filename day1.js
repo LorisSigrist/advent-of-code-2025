a56b3478e530446bfd54e3039f4ad7db9f5a925e
Loris Sigrist <loris@sigrist.dev>
2025-12-01 06:24:16 +0100

    Solve day1

---
+++ day1.js
@@ -0,0 +1,25 @@
+import fs from "node:fs/promises";
+
+const input = await fs.readFile("input_day1.txt", { encoding: "utf-8" });
+
+const rotations = input.split("\n");
+let dial = 50;
+let count = 0;
+
+for (const rotation of rotations) {
+  const dir = rotation.charAt(0);
+  const amount = Number.parseInt(rotation.slice(1));
+
+  // skip empty lines
+  if (Number.isNaN(amount)) continue;
+
+  // fuck it, brute force
+  for (let i = 0; i < amount; i++) {
+    dial += dir == "L" ? -1 : 1;
+    if (dial < 0) dial += 100;
+    if (dial > 99) dial -= 100;
+    if (dial == 0) count++;
+  }
+}
+
+console.log(count);
