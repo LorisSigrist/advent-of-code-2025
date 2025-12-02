1bc0e6d527dc18c979888ebc0e1e5569f4e27f19
Loris Sigrist <loris@sigrist.dev>
2025-12-02 08:55:48 +0100

    Solve day 2

--- day2/day2.js
+++ day2/day2.js
@@ -1,8 +1,34 @@
-import fs from "node:fs/promises"
+import fs from "node:fs/promises";
 
-const input = await fs.readFile("input.txt", { encoding: "utf-8" });
-const lines = input.split("\n");
+const input = await fs.readFile("input.my.txt", { encoding: "utf-8" });
 
-for (const line of lines) { 
-    
+let sum = 0;
+const ranges = input.split(",");
+for (const range of ranges) {
+  const [start, end] = range.split("-").map(Number);
+  if (Number.isNaN(start) || Number.isNaN(end)) continue;
+
+  for (let i = start; i <= end; i++) {
+    if (isInvalid(i)) sum += i;
+  }
+}
+console.log(sum);
+
+function isInvalid(i) {
+  const num = `${i}`; // stringify without leading 0s
+
+  for (let prefixLength = 1; prefixLength <= num.length / 2; prefixLength++) {
+    if (num.length % prefixLength != 0) continue; // skip prefix lengths that dont fit
+
+    const prefix = num.slice(0, prefixLength);
+    let repeated = "";
+    const repeats = num.length / prefixLength;
+    for (let i = 0; i < repeats; i++) {
+      repeated += prefix;
+    }
+
+    if (repeated == num) return true;
+  }
+
+  return false;
 }
